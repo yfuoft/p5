fbd5c5272939dd15f0fe6e7df81f5ec031ee0213
yfuoft <primus@localhost.localdomain>
2022-04-27 01:34:39 -0400

    Updated introduction.

--- init.R
+++ init.R
@@ -3,3 +3,20 @@ install.packages("sf")
 install.packages("opendatatoronto")
 library("httr")
 library("readxl")
+# This code block imports the needed R packages.
+library("httr")
+library("readxl")
+library("tidyverse")
+library(dplyr)
+library(haven)
+library(ggplot2)
+library(bibtex)
+#Assigns datasets to memorable variables
+cd <- read_excel("data/allcurrenciesfinal12.18.17.xlsx")
+cc <- read_csv("data/DP_LIVE_27042022071642777.csv")
+# Filters and gets cumulative metrics by year by taking the per annum average
+cc %>%
+  dplyr::group_by(TIME) %>% 
+  summarise(mean(cc$LOCATION))
+cc %>%
+  filter(LOCATION=='OECD')
